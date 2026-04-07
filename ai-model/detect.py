11e84e6606850f95e226d9dd6b144561ac2fa0ca
Anusha Shivraj Kharvi <anushakharvi015@gmail.com>
2026-04-07 15:35:26 +0530

    AI module: scheduler, detection pipeline, duplicate filtering, image annotation completed

---
+++ ai-model/detect.py
@@ -0,0 +1,18 @@
+import cv2
+import random
+
+def run_detection(frame):
+    height, width, _ = frame.shape
+
+    # Simulate detection ONLY sometimes
+    detected = random.choice([True, False])
+
+    if detected:
+        x1, y1 = int(width*0.3), int(height*0.3)
+        x2, y2 = int(width*0.7), int(height*0.7)
+
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        cv2.putText(frame, "Garbage", (x1, y1-10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,0), 2)
+
+    return detected, frame
